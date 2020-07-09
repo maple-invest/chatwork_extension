7763620058cff1afc6bed6267dace9c989823e14
maple_invest <maple.invest.10@gmail.com>
2020-07-09 19:21:29 +0900

    chatworkでの要素取得と書き換え実装（メッセージ非表示化）

--- background.js
+++ background.js
@@ -2,11 +2,38 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning ' + tab.url + ' red!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
+
+//var elements = document.getElementsByClassName('ctr-p');
+//console.log(elements);
+console.log("aaa!");
+
+window.onload = function(){
+	console.log("bbb!");
+
+	// chatworkのindex.jsでページ内容がロードされてるので、ロード終了まで待つ
+	// sleep処理はイケてないので読み込み終了をリスナーする仕組みにしたい
+	// あるいは一定時間毎に読み込みしなおすとか？
+	setTimeout(function(){
+	     console.log("ccc!");
+	     	$(function(){
+	     		// _messageIdから始まるid要素を取得
+	     	    $("[id^='_messageId']").each(function(){
+	     	        var value = $(this).html();
+	     	        $(this).hide();//非表示化
+
+	     //	        
+	     //	        if(!value) {
+	     //	            return true;
+	     //	        }
+	     //	        
+	     //	        if(value.indexOf('Symfoware') == -1) {
+	     //	            return true;
+	     //	        }
+	     //	        
+	     //	        $(this).html('');
+
+	     	        console.log(value);
+	     	    });
+	     	});
+	},5000);
+}
