e0b62362e2afeeefc34104d029cce5d8a0bc7608
maple_invest <maple.invest.10@gmail.com>
2020-07-10 19:30:28 +0900

    指定行数で文字表示を打ち切る

--- background.js
+++ background.js
@@ -31,6 +31,27 @@ window.onload = function(){
 	     	        	$(this).hide();//非表示化
 	     	        	return true;
 	     	        }
+
+	     	        //長文を折りたたむ
+	     	        var message = $(this).find('pre').html();
+	     	        
+	     	        // 正規表現で改行コードを検索→該当したら配列に追加→lenghtでカウント
+	     	        // 0から数えるので補正値+1をする
+	     	        var targetStr = "\n" ; // \r も必要？
+	     	        var line_count = ( message.match( new RegExp( targetStr, "g" ) ) || [] ).length + 1;
+
+	     	        console.log(message)
+	     	        console.log(message.length)
+	     	        console.log(line_count)
+
+	     	        // 3行目で文字表示を打ち切る書き換え
+	     	        if(line_count > 3){
+	     	        	var line_1 = message.indexOf(targetStr);
+	     	        	var line_2 = message.indexOf(targetStr, line_1 + 1);
+	     	        	var line_3 = message.indexOf(targetStr, line_2 + 1);
+
+	     	        	$(this).find('pre').html(message.slice( 0, line_3 ));
+	     	        }
 	     	        
 
 	     //	        
@@ -50,5 +71,5 @@ window.onload = function(){
 	     	    console.log("count!");
 	     	    console.log(counter);
 	     	});
-	},5000);
+	},3000);
 }
